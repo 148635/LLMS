de0cebb903ded533fa11eef3ee71c54e09c44b6d
148635 <83212036+148635@users.noreply.github.com>
2025-07-19 17:57:43 +0530

    Update Constants.java

--- src/main/java/com/llms/model/utils/Constants.java
+++ src/main/java/com/llms/model/utils/Constants.java
@@ -5,5 +5,5 @@ public final class Constants {
     public static final String EXCEPTION_MSG = "Oops..! Looks like there is an issue. ";
     public static final String GROQ_ERROR_PREFIX = "Groq error: ";
     public static final String GEMINI_ERROR_PREFIX = "Gemini error: ";
-
+    public static final String TOGETHER_ERROR_PREFIX = "Together error : ";
 }
