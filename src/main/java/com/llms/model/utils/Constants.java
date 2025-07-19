0fa9211a8b608f3538b55c50dd3ed7c85599159d
148635 <vvelpula356@.com>
2025-07-19 18:01:09 +0530

    removed old commits

--- src/main/java/com/llms/model/utils/Constants.java
+++ src/main/java/com/llms/model/utils/Constants.java
@@ -6,4 +6,6 @@ public final class Constants {
     public static final String GROQ_ERROR_PREFIX = "Groq error: ";
     public static final String GEMINI_ERROR_PREFIX = "Gemini error: ";
 
+    public static final String TOGETHER_ERROR_PREFIX = "Together error : ";
+
 }
